68260570d0e008c361c1950d16c75fab500af1cc
dana-tal <46241925+dana-tal@users.noreply.github.com>
2024-10-16 17:43:55 +0300

    questions.js

---
+++ src/utils/questions.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: 'q1',
+    text: 'Which of the following definitions best describes React.js?',
+    answers: {
+     a1:'A library to build user interfaces with help of declarative code.',
+     a2:'A library for managing state in web applications.',
+     a3:'A framework to build user interfaces with help of imperative code.',
+     a4:'A library used for building mobile applications only.'
+    }
+  },
+  {
+    id: 'q2',
+    text: 'What purpose do React hooks serve?',
+    answers: {
+     a1:'Enabling the use of state and other React features in functional components.',
+     a2:'Creating responsive layouts in React applications.',
+     a3:'Handling errors within the application.',
+     a4:'Part of the Redux library for managing global state.',
+    }
+  },
+  {
+    id: 'q3',
+    text: 'Can you identify what JSX is?',
+    answers: {
+     a1:'A JavaScript extension that adds HTML-like syntax to JavaScript.',
+     a2:'A JavaScript library for building dynamic user interfaces.',
+     a3:'A specific HTML version that was explicitly created for React.',
+     a4:'A tool for making HTTP requests in a React application.',      
+    }
+  },
+  {
+    id: 'q4',
+    text: 'What is the most common way to create a component in React?',
+    answers: {
+      a1:'By defining a JavaScript function that returns a renderable value.',
+      a2:'By defining a custom HTML tag in JavaScript.',
+      a3:'By creating a file with a .jsx extension.',
+      a4:'By using the "new" keyword followed by the component name.',
+    } 
+  },
+  {
+    id: 'q5',
+    text: 'What does the term "React state" imply?',
+    answers: {
+     a1:'An object in a component that holds values and may cause the component to render on change.',
+     a2:'The lifecycle phase a React component is in.',
+     a3:'The overall status of a React application, including all props and components.',
+     a4:'A library for managing global state in React applications.',
+    }
+  },
+  {
+    id: 'q6',
+    text: 'How do you typically render list content in React apps?',
+    answers: {
+      a1:'By using the map() method to iterate over an array of data and returning JSX.',
+      a2:'By using the for() loop to iterate over an array of data and returning JSX.',
+      a3:'By using the forEach() method to iterate over an array of data and returning JSX.',
+      a4:'By using the loop() method to iterate over an array of data and returning JSX.',
+    }
+  },
+  {
+    id: 'q7',
+    text: 'Which approach can NOT be used to render content conditionally?',
+    answers: {
+      a1:'Using a the #if template syntax.',
+      a2:'Using a ternary operator.',
+      a3:'Using the && operator.',
+      a4:'Using an if-else statement.',
+    } 
+   },
+];
