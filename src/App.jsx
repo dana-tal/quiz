ae0e1f9b230dba12f8e3021fd96c150cf4769bd6
dana-tal <46241925+dana-tal@users.noreply.github.com>
2024-10-16 17:37:35 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header.jsx';
+import Quiz from './components/Quiz.jsx';
+
+function App() {
+    return(
+        <main>
+            <Header />
+            <Quiz />
+    </main>
+  );
+}
+
+export default App;
